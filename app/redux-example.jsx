bb287d88fb72436dc185e50f20c8cd2751e82e20
Thor Erik Lie <thor@thorerik.com>
2016-11-17 20:49:18 +0100

    split everything into smaller modules

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,150 +1,10 @@
-var redux = require('redux');
-var axios = require('axios');
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
 
 cl('Starting redux example');
 
-// Name reducer and action generators
-// ----------------------------------
-var nameReducer = (state = 'Anonymous', action) => {
-  switch(action.type) {
-    case 'CHANGE_NAME':
-      return action.name;
-    default:
-      return state;
-  }
-};
-
-var changeName = (name) => {
-  return {
-    type: 'CHANGE_NAME',
-    name
-  };
-};
-
-// Hobbies reducer and action generators
-// -------------------------------------
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_HOBBY':
-      return [
-        ...state,
-        {
-          id: nextHobbyId++,
-          hobby: action.hobby
-        }
-      ];
-    case 'REMOVE_HOBBY':
-      return state.filter((item) => item.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-var addHobby = (hobby) => {
-  return {
-    type: 'ADD_HOBBY',
-    hobby
-  };
-};
-
-var removeHobby = (id) => {
-  return {
-    type: 'REMOVE_HOBBY',
-    id
-  };
-};
-
-// Movies reducer and action generators
-// ------------------------------------
-var nextMovieId = 1;
-var moviesReducer = (state = [], action) => {
-  switch(action.type) {
-    case 'ADD_MOVIE':
-      return [
-          ...state,
-          {
-            id: nextMovieId++,
-            title: action.title,
-            genre: action.genre
-          }
-        ];
-    case 'REMOVE_MOVIE':
-      return state.filter((item) => item.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-var addMovie = (title, genre) => {
-  return {
-    type: 'ADD_MOVIE',
-    title,
-    genre
-  };
-};
-
-var removeMovie = (id) => {
-  return {
-    type: 'REMOVE_MOVIE',
-    id
-  };
-};
-
-// Map reducer and action generators
-// ---------------------------------
-var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
-  switch (action.type) {
-    case 'START_LOCATION_FETCH':
-      return {
-        isFetching: true,
-        url: undefined
-      };
-    case 'COMPLETE_LOCATION_FETCH':
-      return {
-        isFetching: false,
-        url: action.url
-      };
-    default:
-      return state;
-  }
-};
-
-var startLocationFetch = () => {
-  return {
-    type: 'START_LOCATION_FETCH'
-  };
-};
-
-var completeLocationFetch = (url) => {
-  return {
-    type: 'COMPLETE_LOCATION_FETCH',
-    url
-  };
-};
-
-var fetchLocation = () => {
-  store.dispatch(startLocationFetch());
-
-  axios.get('http://ipinfo.io').then(function (res) {
-    var loc = res.data.loc;
-    var baseUrl = 'http://maps.google.com/?q=';
-
-    store.dispatch(completeLocationFetch(baseUrl + loc));
-  });
-};
-
 // -------------------
-var reducer = redux.combineReducers({
-  name: nameReducer,
-  hobbies: hobbiesReducer,
-  movies: moviesReducer,
-  map: mapReducer
-});
 
-var store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-));
 
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
@@ -158,20 +18,20 @@ var unsubscribe = store.subscribe(() => {
 
 // -------------------
 
-store.dispatch(changeName('Mrow'));
+store.dispatch(actions.fetchLocation());
 
-store.dispatch(addHobby('Eat'));
-store.dispatch(addHobby('Sleep'));
-store.dispatch(removeHobby(2));
+store.dispatch(actions.changeName('Mrow'));
 
-store.dispatch(changeName('Meow'));
+store.dispatch(actions.addHobby('Eat'));
+store.dispatch(actions.addHobby('Sleep'));
+store.dispatch(actions.removeHobby(2));
 
-store.dispatch(addMovie('Mad Max', 'Action'));
-store.dispatch(addMovie('Care Bears The Movie', 'Horror'));
-store.dispatch(removeMovie(1));
+store.dispatch(actions.changeName('Meow'));
 
+store.dispatch(actions.addMovie('Mad Max', 'Action'));
+store.dispatch(actions.addMovie('Care Bears The Movie', 'Horror'));
+store.dispatch(actions.removeMovie(1));
 
-fetchLocation();
 
 //unsubscribe();
 
